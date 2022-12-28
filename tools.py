5cca743865f46befcfd1404c499ce6d673e27b7c
batophobia <godrox3@yahoo.com>
2022-12-28 15:58:02 -0600

    Added docstrings

--- tools.py
+++ tools.py
@@ -1,12 +1,15 @@
 import os
 
 def cls():
+  """Clear the console"""
   os.system('cls' if os.name == 'nt' else 'clear')
 
 def checkKey(dic, key):
+  """Check if a given dictionary contains a specific key"""
   return key in dic.keys()
 
 def isNumber(val):
+  """Check if a given value is a number"""
   try:
     float(val)
     return True
@@ -14,12 +17,14 @@ def isNumber(val):
     return False
 
 def getNum(message):
+  """Repeat input until given a valid number"""
   userInput = ""
   while not isNumber(userInput):
     userInput = input(message)
   return float(userInput)
 
 def getOneOfThese(message, options):
+  """Repeat input until given one of a list of options"""
   userInput = "ݧܔthis is ㍌definitely not an ⣯option in the list of options♥ꔰ"
   while userInput not in options:
     userInput = input(message)
