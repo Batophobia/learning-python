19b7f49dc57e98643f4de58c1b5b896be588c5e0
batophobia <godrox3@yahoo.com>
2024-04-15 09:46:17 -0500

    Simple python script for testing remote pulls

--- 000-test.py
+++ 000-test.py
@@ -1,9 +1,23 @@
-from PIL import Image
+# from PIL import Image
 
-IMG_W = 600
-IMG_H = 600
-image = Image.open(r"D:\tmp\dpOfh.png")
-image = image.resize((IMG_W, IMG_H))
-imgColors = Image.Image.getcolors(image,360000)
-imgColors.sort(reverse=True)
-print(imgColors)
+# IMG_W = 600
+# IMG_H = 600
+# image = Image.open(r"D:\tmp\dpOfh.png")
+# image = image.resize((IMG_W, IMG_H))
+# imgColors = Image.Image.getcolors(image,360000)
+# imgColors.sort(reverse=True)
+# print(imgColors)
+
+import random
+OUTFILE="./output.txt"
+
+def run():
+  num = random.randint(0,10)
+  with open(OUTFILE, mode="w") as file:
+      file.write(str(num))
+
+def main():
+  run()
+
+if __name__ == '__main__':
+  main()
